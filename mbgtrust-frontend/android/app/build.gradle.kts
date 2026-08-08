b36e509c10498a7da3df619c208ea1a7d662fba3
Fuad Dhiyaulhaq <fuad.dhiyaulhaq@gmail.com>
2026-08-08 17:32:52 +0700

    fix: downgrade flutter_secure_storage to 9.2.4 and compileSdk 35 for android build

--- mbgtrust-frontend/android/app/build.gradle.kts
+++ mbgtrust-frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.mbgtrust.mbgtrust"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
